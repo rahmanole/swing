280e36529cfb0d48feb0c232a495c4f0da3cc67f
Minhaz <rahmanoole.be@gmail.com>
2019-04-17 03:36:23 +0600

    up

--- Evedence/src/pack/ProductDAOImp.java
+++ Evedence/src/pack/ProductDAOImp.java
@@ -15,125 +15,126 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class ProductDAOImp {
-    public static Connection connectDB(){
-        Connection conn =null;
+
+    public static Connection connectDB() {
+        Connection conn = null;
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/evedence","root","1234");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/evedence", "root", "1234");
         } catch (SQLException ex) {
             Logger.getLogger(ProductDAOImp.class.getName()).log(Level.SEVERE, null, ex);
         }
         return conn;
     }
-    
-    String sql = "create table product(id int primary key auto_increment,name varchar(55),stock int,price double)";
-    
-    public Product getProductById(int id){
-      Product product = null;
-      String stmt = "select * from product where id=?";
-      
-      Connection conn = connectDB();
-      
+    //this stmt for creating table through mysql command line
+    //create table product(id int primary key auto_increment,name varchar(55),stock int,price double)
+
+//this stmt to insert data into table through mysql command line
+    //"insert into product(name,stock,price) values(\"pen\",100,10)
+    public Product getProductById(int id) {
+        Product product = null;
+        String stmt = "select * from product where id=?";
+
+        Connection conn = connectDB();
+
         try {
             PreparedStatement ps = conn.prepareStatement(stmt);
-            ps.setInt(1,id);
+            ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){
-                product = new Product(rs.getInt(1),rs.getString(2), rs.getInt(3), rs.getDouble(4));
+            while (rs.next()) {
+                product = new Product(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getDouble(4));
             }
-            
+
         } catch (Exception e) {
             e.printStackTrace();
-        }finally{
-          try {
-              conn.close();
-          } catch (SQLException ex) {
-              Logger.getLogger(ProductDAOImp.class.getName()).log(Level.SEVERE, null, ex);
-          }
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(ProductDAOImp.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-      
-      return product;
+
+        return product;
     }
-    
+
     //This method for all product list
-    public List<Product> getProductList(){
-      List<Product> plist = new ArrayList<>();
-      String stmt = "select * from product";
-      
-      Connection conn = connectDB();
-      
+    public List<Product> getProductList() {
+        List<Product> plist = new ArrayList<>();
+        String stmt = "select * from product";
+
+        Connection conn = connectDB();
+
         try {
             PreparedStatement ps = conn.prepareStatement(stmt);
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){
-                plist.add(new Product(rs.getInt(1),rs.getString(2), rs.getInt(3), rs.getDouble(4)));
+            while (rs.next()) {
+                plist.add(new Product(rs.getInt(1), rs.getString(2), rs.getInt(3), rs.getDouble(4)));
             }
-            
+
         } catch (Exception e) {
             e.printStackTrace();
-        }finally{
-          try {
-              conn.close();
-          } catch (SQLException ex) {
-              Logger.getLogger(ProductDAOImp.class.getName()).log(Level.SEVERE, null, ex);
-          }
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(ProductDAOImp.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-      
-      return plist;
+
+        return plist;
     }
-    
+
     //This method for finding current stock
-    public int getStockById(int id){
-      int stock = 0;
-      String stmt = "select stock from product where id=?";
-      
-      Connection conn = connectDB();
-      
+    public int getStockById(int id) {
+        int stock = 0;
+        String stmt = "select stock from product where id=?";
+
+        Connection conn = connectDB();
+
         try {
             PreparedStatement ps = conn.prepareStatement(stmt);
-            ps.setInt(1,id);
+            ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 stock = rs.getInt(1);
             }
-            
+
         } catch (Exception e) {
             e.printStackTrace();
-        }finally{
-          try {
-              conn.close();
-          } catch (SQLException ex) {
-              Logger.getLogger(ProductDAOImp.class.getName()).log(Level.SEVERE, null, ex);
-          }
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(ProductDAOImp.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-      
-      return stock;
+
+        return stock;
     }
-    
+
     //This method will update product stock when sell or purchase will occur
-    public void updateStock(int stock,int id){
-      String stmt = "update product set stock = ? where id=?";
-      
-      Connection conn = connectDB();
-      
+    public void updateStock(int stock, int id) {
+        String stmt = "update product set stock = ? where id=?";
+
+        Connection conn = connectDB();
+
         try {
             PreparedStatement ps = conn.prepareStatement(stmt);
-            ps.setInt(1,stock);
+            ps.setInt(1, stock);
             ps.setInt(2, id);
             ps.execute();
-            
-            
+
         } catch (Exception e) {
             e.printStackTrace();
-        }finally{
-          try {
-              conn.close();
-          } catch (SQLException ex) {
-              Logger.getLogger(ProductDAOImp.class.getName()).log(Level.SEVERE, null, ex);
-          }
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(ProductDAOImp.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-      
+
     }
-    
+
 }
